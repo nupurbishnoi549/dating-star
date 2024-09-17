db783fb14af279c6ef69fd323a42782d71f0ef4b
Your Name <you@example.com>
2024-09-17 16:59:39 -0700

    done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,8 @@
-$(".slider").slick({
-    dots: false,
-    infinite: true,
-    speed: 300,
+$('.slider').slick({
     slidesToShow: 3,
     slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
     responsive: [
         {
             breakpoint: 1440,
@@ -28,35 +27,6 @@ $(".slider").slick({
                 slidesToScroll: 1,
             },
         },
-    ],
-});
-
-
-$(".slick_slider").slick({
-    dots: true,
-    infinite: true,
-    speed: 300,
-    slidesToShow: 2,
-    slidesToScroll: 1,
-    nextArrow: ".next",
-    prevArrow: ".prev",
-    responsive: [
-        {
-            breakpoint: 1440,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 1,
-                infinite: true,
-                dots: true,
-            },
-        },
-        {
-            breakpoint: 768,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 1,
-            },
-        },
         {
             breakpoint: 576,
             settings: {
@@ -64,6 +34,23 @@ $(".slick_slider").slick({
                 slidesToScroll: 1,
             },
         },
-
     ],
+});
+$('.second-slider').slick({
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2500,
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 1
+            }
+        },
+
+    ]
 });
